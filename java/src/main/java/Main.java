76e35001c11c74a3fe4715c37816750cf1a31bd0
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2017-08-15 12:51:40 +0800

    add simple java project

---
+++ java/src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void mp(int n) {
+        System.out.println(n * n);
+    }
+}
---
+++ java/src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+package test.java;
+
+import main.java.Main;
+
+class MainTest {
+    public static void main(String[] args) {
+        Main.mp(10);
+    }
+}
